32b3591ec899989e353ea37fb6aa4b08deed6a70
Ananya <goyalananya444@gmail.com>
2023-05-31 20:14:34 +0530

    opportunities-page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Homepage from './pages/Homepage';
+import Opportunities from './pages/Opportunities';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
       <Route path="/" element={<Homepage/>} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/opportunities" element={<Opportunities/>} />
         <Route path="/signup" element={<Signup/>} />
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/Opportunities.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavbarH from '../components/NavbarH'
+import Carousel from '../components/Carousel'
+import MenuBar from '../components/MenuBar'
+import Footer from '../components/Footer'
+
+export default function Opportunities() {
+  return (
+    <div>
+      <NavbarH/>
+      <Carousel/>
+      <MenuBar/>
+      <Footer/>
+    </div>
+  )
+}
